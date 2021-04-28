66c4a8d7332fbe0d07fab17500a2c534ef678104
Fun Planet <dipamdiptam@gmail.com>
2021-04-28 21:48:56 +0530

    add syllabus link

--- src/screens/ExamMaterialScreen.tsx
+++ src/screens/ExamMaterialScreen.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 
 const ExamMaterial: React.FC = () => {
   return (
     <div>
       <h2>Exam: Pre Mid Term Test</h2>
+      <Button
+        color="primary"
+        variant="outlined"
+        href="https://drive.google.com/file/d/1w1MuGPGwFnfsw_hczd6_7LNzy_jdsuIK/view?usp=sharing"
+        target="_blank"
+      >
+        Syllabus
+      </Button>
     </div>
   );
 };
